44cdccd133834a5944b0030e2d7a069fe8f44fb9
Yoshihiro Tanaka <contact@cordea.jp>
2022-06-05 12:40:55 +0900

    build html

--- main.go
+++ main.go
@@ -10,10 +10,30 @@ import (
 	"os/exec"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+type server struct {
+	qr string
+}
+
+func (s *server) buildHtml(content string) string {
+	font := "<link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">" +
+		"<link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>" +
+		"<link href=\"https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap\" rel=\"stylesheet\">"
+	style := "style=\"text-align: center;margin: 16px;\""
+	return fmt.Sprintf("<html><head>%s</head><body %s>%s</body></html>", font, style, content)
+}
+
+func (s *server) handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Detected access!")
 	fmt.Println(r.Header.Get("User-Agent"))
-	if _, err := fmt.Fprint(w, "<html></html>"); err != nil {
+	html := s.buildHtml(fmt.Sprintf("<pre>%s</pre>", s.qr))
+	if _, err := fmt.Fprint(w, html); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func (s *server) serve() {
+	http.HandleFunc("/", s.handler)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatalln(err)
 	}
 }
@@ -33,11 +53,8 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	fmt.Println(qr.String())
 
 	fmt.Println("Starting the web server...")
-	http.HandleFunc("/", handler)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatalln(err)
-	}
+	s := server{qr: qr.String()}
+	s.serve()
 }
